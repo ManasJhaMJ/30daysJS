ee4197648dce4672e532dbf08e0a6a9a427402eb
manasjhamj <jhamanas939@gmail.com>
2024-07-13 22:53:57 +0530

    day01

---
+++ day01/01.js
@@ -0,0 +1,36 @@
+// Task 01
+
+var a = 4;
+console.log(a);
+
+// Task 02
+let b = 'Hello Nika'
+console.log(b);
+
+// Task 03
+const c = true;
+console.log(c);
+
+// task 04
+console.log(typeof ("Jonny" + "sins"));
+console.log(typeof 69);
+console.log(typeof true);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof null);
+console.log(typeof { a, b, c });
+
+// Task 05
+let d = 6;
+console.log(d);
+
+d = 9;
+console.log(d);
+
+// Task 06
+const messi = 10;
+console.log(messi);
+
+messi = 7;
+console.log(messi);  //Error : Assignment to constant variable
